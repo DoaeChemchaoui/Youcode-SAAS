a26455b5563f655e5f4d01311da70fe749a6cc30
doae <chemchaouidoae@gmail.com>
2025-09-04 00:48:28 +0100

    Voici le premier mini projet de Système de Gestion de Stock dans une Librairie

--- miniprojet/stock.c
+++ miniprojet/stock.c
@@ -42,6 +42,73 @@ int afficherLivres(int nombreLivres, char titres[][100], char auteurs[][100], fl
     return nombreLivres; 
 }
 
+int rechercherLivre(int nombreLivres, char titres[][100], char auteurs[][100], float prix[], int quantites[]) {
+    char recherche[100];
+    printf("Entrez le titre du livre à rechercher : ");
+    scanf("%s", recherche);
+
+    for (int i = 0; i < nombreLivres; i++) {
+        if (strcmp(titres[i], recherche) == 0) {
+            printf("\n Livre trouvé :\n");
+            printf("Titre : %s\n", titres[i]);
+            printf("Auteur : %s\n", auteurs[i]);
+            printf("Prix : %.2f\n", prix[i]);
+            printf("Quantité : %d\n", quantites[i]);
+            return 1;
+        }
+    }
+
+    printf("Livre non trouvé.\n");
+    return 0;
+}
+
+int mettreAJourQuantite(int nombreLivres, char titres[][100], int quantites[]) {
+    char recherche[100];
+    printf("Entrez le titre du livre à mettre à jour : ");
+    scanf("%s", recherche);
+    for (int i = 0; i < nombreLivres; i++) {
+        if (strcmp(titres[i], recherche) == 0) {
+            printf("Nouvelle quantité : ");
+            scanf("%d", &quantites[i]);
+            printf("Quantité mise à jour avec succès !\n");
+            return 1;
+        }
+    }
+    printf("Livre non trouvé.\n");
+    return 0;
+}
+
+int supprimerLivre(int nombreLivres, char titres[][100], char auteurs[][100], float prix[], int quantites[]) {
+    char recherche[100];
+    printf("Entrez le titre du livre à supprimer : ");
+    scanf("%s", recherche);
+
+    for (int i = 0; i < nombreLivres; i++) {
+        if (strcmp(titres[i], recherche) == 0) {
+            for (int j = i; j < nombreLivres - 1; j++) {
+                strcpy(titres[j], titres[j + 1]);
+                strcpy(auteurs[j], auteurs[j + 1]);
+                prix[j] = prix[j + 1];
+                quantites[j] = quantites[j + 1];
+            }
+            printf("Livre supprimé avec succès.\n");
+            return nombreLivres - 1;
+        }
+    }
+
+    printf("Livre non trouvé.\n");
+    return nombreLivres;
+}
+
+int totalLivres(int nombreLivres, int quantites[]) {
+    int total = 0;
+    for (int i = 0; i < nombreLivres; i++) {
+        total += quantites[i];
+    }
+    printf("Nombre total de livres en stock : %d\n", total);
+    return total;
+}
+
 int main() {
     int choix;
     int nombreLivres = 0;
@@ -70,19 +137,16 @@ int main() {
                 afficherLivres(nombreLivres, titres, auteurs, prix, quantites);
                 break;
             case 3:
-                printf("Rechercher \n");
+                rechercherLivre(nombreLivres, titres, auteurs, prix, quantites);
                 break;
             case 4:
-                printf("Mise à jour\n");
+                mettreAJourQuantite(nombreLivres, titres, quantites);
                 break;
             case 5:
-                printf("Suppression\n");
+                nombreLivres = supprimerLivre(nombreLivres, titres, auteurs, prix, quantites);
                 break;
             case 6:
-                printf("Total en stock\n");
-                break;
-            case 7:
-                printf("Au revoir!\n");
+                totalLivres(nombreLivres, quantites);
                 break;
             default:
                 printf("Choix invalide, réessayez.\n");
