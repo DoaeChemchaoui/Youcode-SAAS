307051f2010e9cae2ce09c96a528dd69d177a50f
Doae <chemchaouidoae@gmail.com>
2025-09-08 14:51:13 +0100

    Conversion en Majuscules

---
+++ day-03-challenge/strings/str-chall7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char chaine[100];
+
+    printf("Entrez une chaine : ");
+    scanf("%s", chaine);
+    printf("les caracteres en majuscules: %s",strupr(chaine));
+    
+}
