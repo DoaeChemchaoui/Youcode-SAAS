e5ee849dc819e7e0af2dbea85671a0d54b53d338
Doae <chemchaouidoae@gmail.com>
2025-09-08 14:48:27 +0100

    Affichage d'une Chaîne

---
+++ day-03-challenge/strings/str-chall1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char chaine[100];
+
+    printf("Entrez une chaine : ");
+    scanf("%s", chaine);
+    
+    printf("Vous avez entre : %s\n", chaine);
+
+    return 0;
+}
+
+
