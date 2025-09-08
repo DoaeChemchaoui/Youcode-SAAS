badc1a6fffc4e203797c283511b723d5d4151ada
Doae <chemchaouidoae@gmail.com>
2025-09-08 18:50:31 +0100

    Définition et Utilisation de Structure

---
+++ day-03-challenge/structs/struct-chall1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char nom[] = "chemchaoui";
+    char prenom[] = "doae";
+    int age = 19;
+
+    printf("Nom : %s\n", nom);
+    printf("Prenom : %s\n", prenom);
+    printf("Age : %d\n", age);
+
+    return 0;
+}
